624f824b0e39b80c9faffdcd840ae953491790b5
12066 <12066@DESKTOP-VRP6PFT>
2021-02-26 16:46:48 -0500

    New Commit

--- src/Test2.java
+++ src/Test2.java
@@ -3,7 +3,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello second Git test");
-		System.out.println("New comment in test 2");
+		System.out.println("New comment in test 2 ");
 		
 	}
 
